40293f097fcf8e221656ff2f7240e45aa48e7549
asru-islam <arasru01@gmail.com>
2023-06-07 03:36:50 +0600

    create a jwt secret code and verify the jwt code

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ const port = process.env.PORT | 5000;
 app.use(express.json());
 app.use(cors());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w00ka8a.mongodb.net/?retryWrites=true&w=majority`;
 
